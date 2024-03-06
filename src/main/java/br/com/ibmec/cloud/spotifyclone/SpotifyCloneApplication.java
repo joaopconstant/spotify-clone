7b0a58b5aa596bcad78fd75b230acc7799dabe77
JOAO PEDRO TAKAMOTO CONSTANT <202208386288@alunos.ibmec.edu.br>
2024-03-06 09:06:44 -0300

    Initial commit

---
+++ src/main/java/br/com/ibmec/cloud/spotifyclone/SpotifyCloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibmec.cloud.spotifyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ibmec/cloud/spotifyclone/SpotifyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibmec.cloud.spotifyclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
